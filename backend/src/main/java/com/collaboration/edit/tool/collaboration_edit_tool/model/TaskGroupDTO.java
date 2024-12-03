0307d8a68f4538f56923264567b17d27bf8b191a
Christopher-NguyenCS <69370834+Christopher-NguyenCS@users.noreply.github.com>
2024-12-02 21:06:59 -0500

    - Fix spacing to make it more readable.

--- backend/src/main/java/com/collaboration/edit/tool/collaboration_edit_tool/model/TaskGroupDTO.java
+++ backend/src/main/java/com/collaboration/edit/tool/collaboration_edit_tool/model/TaskGroupDTO.java
@@ -1,6 +1,10 @@
 package com.collaboration.edit.tool.collaboration_edit_tool.model;
 
+import java.util.List;
 import java.util.UUID;
+
+import com.collaboration.edit.tool.collaboration_edit_tool.data.entity.Task;
+
 
 public class TaskGroupDTO {
 
@@ -9,5 +13,7 @@ public class TaskGroupDTO {
     public String groupTitle;
    
     public String groupDescription;
+
+    public List<Task> tasks;
     
 }
