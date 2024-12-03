836baea5d55fa64e47ea5666a4ac98c257b55089
Christopher-NguyenCS <69370834+Christopher-NguyenCS@users.noreply.github.com>
2024-12-02 20:44:04 -0500

    - Add validation to check on request body. - Add exception checks when an id does not exist in the database.

--- backend/src/main/java/com/collaboration/edit/tool/collaboration_edit_tool/services/TaskGroupService.java
+++ backend/src/main/java/com/collaboration/edit/tool/collaboration_edit_tool/services/TaskGroupService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.collaboration.edit.tool.collaboration_edit_tool.data.entity.TaskGroup;
 import com.collaboration.edit.tool.collaboration_edit_tool.data.repository.TaskGroupRepository;
 import com.collaboration.edit.tool.collaboration_edit_tool.data.repository.TaskRepository;
+import com.collaboration.edit.tool.collaboration_edit_tool.exception.validator.TaskGroupValidator;
 import com.collaboration.edit.tool.collaboration_edit_tool.model.TaskGroupDTO;
 
 @Service
@@ -16,20 +18,23 @@ public class TaskGroupService {
     TaskGroupRepository taskGroupRepository;
     TaskRepository taskRepository;
 
+
     public TaskGroupService(TaskGroupRepository taskGroupRepository, TaskRepository taskRepository){
         this.taskGroupRepository = taskGroupRepository;
         this.taskRepository = taskRepository;
+
     }
 
     public List<TaskGroup> getAllTaskGroups() {
         return taskGroupRepository.findAll();
     }
 
-    public Optional<TaskGroup> getTaskGroup(UUID id) {
-        return taskGroupRepository.findById(id);  
+    public TaskGroup getTaskGroup(UUID id) {
+        return taskGroupRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Task Group was not found because id did not exist"));
     }
 
     public void addTaskGroup(TaskGroupDTO taskGroupBody) {
+        TaskGroupValidator.validateTaskGroup(taskGroupBody.groupId, taskGroupBody.groupDescription, taskGroupBody.groupTitle);
         TaskGroup newTaskGroup = new TaskGroup();
         newTaskGroup.setGroupId(taskGroupBody.groupId);
         newTaskGroup.setGroupTitle(taskGroupBody.groupTitle);
@@ -37,16 +42,13 @@ public class TaskGroupService {
         taskGroupRepository.save(newTaskGroup);
     }
 
-    public boolean updateTaskGroup(UUID id, TaskGroupDTO taskGroupBody){
+    public void updateTaskGroup(UUID id, TaskGroupDTO taskGroupBody){
+       TaskGroupValidator.validateTaskGroup(taskGroupBody.groupId, taskGroupBody.groupDescription, taskGroupBody.groupTitle);
         Optional<TaskGroup> optionalTaskGroup = taskGroupRepository.findById(id);
-        if(optionalTaskGroup.isPresent()){
-            TaskGroup taskGroup = optionalTaskGroup.get();
-            taskGroup.setGroupDescription(taskGroupBody.groupDescription);
-            taskGroup.setGroupTitle(taskGroupBody.groupTitle);
-            taskGroupRepository.save(taskGroup);
-            return true;
-        }
-        return false;
+        TaskGroup taskGroup = optionalTaskGroup.orElseThrow(()-> new ResourceNotFoundException("Unable to update taskGroup because id did not exist."));
+        taskGroup.setGroupDescription(taskGroupBody.groupDescription);
+        taskGroup.setGroupTitle(taskGroupBody.groupTitle);
+        taskGroupRepository.save(taskGroup);
     }
 
     public void deleteTaskGroup(UUID id) {
@@ -56,7 +58,6 @@ public class TaskGroupService {
             }
             taskGroupRepository.deleteById(id);
          }
-
     }
 
 }
