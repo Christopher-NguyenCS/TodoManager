d6f3bf6d568fd9356abb4346fbb1739689a4ce66
Christopher Nguyen <christophernng1999@gmail.com>
2024-12-10 18:18:25 -0500

    - Initial commit for AppRoute

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,27 +3,10 @@ import "../index.css";
 import { useLoaderData } from 'react-router';
 
 
-export async function loader({params}){
-  const response = await fetch("http://localhost:8080/users");
-  const userData = await response.json();
-  return userData;
-}
 export default function App() {
   const [users, setUsers] = useState();
   const [tasks,setTasks] = useState();
-  // useEffect(()=>{
-  //   const fetchUsers = async()=>{
-  //     try {
-  //       const response = await fetch("http://localhost:8080/users");
-  //       const users = await response.json();
-  //       setUsers(users);
-        
-  //     } catch (error) {
-  //       return <div>{error}</div>
-  //     }
-  //   }
-  //   fetchUsers();
-  // },[]);
+
 
   useEffect(()=>{
     const {userData} = useLoaderData();
