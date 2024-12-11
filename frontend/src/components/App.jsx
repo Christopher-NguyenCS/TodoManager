3b515a61ce42dd59b96167e879dd9bc41d9bc71f
Christopher Nguyen <christophernng1999@gmail.com>
2024-12-10 19:23:41 -0500

    - Fix routing between main and App components - Add two new components for dealing with tasks

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,60 +1,26 @@
-import { useState,useEffect } from 'react'
-import "../index.css";
-import { useLoaderData } from 'react-router';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import AppLayout from './AppLayout';
+import Dashboard from './Dashboard';
+import Project from './Project';
+import TaskGroup from './TaskGroup';
+import Task from './Task';
 
-
-export default function App() {
-  const [users, setUsers] = useState();
-  const [tasks,setTasks] = useState();
-
-
-  useEffect(()=>{
-    const {userData} = useLoaderData();
-    setUsers(userData);
-  },[]);
-
-  useEffect(()=>{
-    const fetchTasks = async() =>{
-      try {
-        const response = await fetch("http://localhost:8080/tasks");
-        const tasks = await response.json();
-        console.log("Tasks:",tasks);
-        setTasks(tasks);
-      } catch (error) {
-        return <div>{error}</div>
-      }
-    }
-    fetchTasks();
-  },[]);
-
-  if(!users){
-    return <div>no users</div>
-  }
-
+const App = () => {
   return (
-    <>
-      <div className="bg-white flex ">
-        <ul>
-          {users.map(user =>(
-            <li className="odd:bg-red-50 even:bg-blue-50 "key={user.id}>{user.firstName}</li>
-          ))}
-        </ul>
-      </div>
 
-      <div className="bg-pink-100 flex">
-        {
-          tasks && tasks.length>0 ? 
-            <ul>
-              {tasks.map(task => (
-              <li className="odd:bg-red-50 even:bg-blue-50" key={task.id}>{task.title}</li>
-              ))}
-            </ul>
-          :
-          <div>not able to grab tasks</div>
-        }
-        </div>
-    </>
-  )
-}
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="project/:projectId" element={<Project />}>
+            <Route path="taskgroup/:taskGroupId" element={<TaskGroup />} >
+              <Route path="task/:taskId" element={<Task />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
 
+  );
+};
 
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes, Route} from "react-router";
-import './index.css'
-import App from './components/App.jsx'
-import AppLayout from './components/AppLayout.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import Project from './components/Project.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route} from "react-router";
+import App from "../src/components/App";
 
 const root = document.getElementById("root");
+
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App/>}>
-        <Route element={<AppLayout/>}>
-          <Route element={<Dashboard/>}>
-              <Route path="project/:projectId" element={<Project/>}>
-              </Route>
-          </Route>
-        </Route>
-      </Route>
+      <Route path="*" element={<App />} />
     </Routes>
   </BrowserRouter>
-)
+);
