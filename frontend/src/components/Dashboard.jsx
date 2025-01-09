bb3b03ff270bdd426c52d3dad7e74de14cad0f46
Christopher Nguyen <christophernng1999@gmail.com>
2025-01-09 01:09:15 -0500

    Initialize Dashboard Skeleton

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,12 @@
+
+//the dashboard should display the 
 export default function Dashboard(){
     return(
-        <div>
-            Dashboard
+        <div className="flex md:grow bg-blue-400  md:w-11/12">
+            <div className="flex justify-center items-center bg-pink-700 md:h-1/5 md:w-80 p-5 m-5">
+                TITLE OF SOME OBJECT TO SEE HOW IT WILL FIT
+            </div>
+            
         </div>
     )
 }
